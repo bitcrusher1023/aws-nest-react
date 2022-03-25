1d5b9b1313aa7f26f7c056a2ee694170a6e6ebbf
David Ng <david.ng.dev@gmail.com>
2022-03-25 08:53:07 +0000

    refactor: remove unnecessary pagination that designed for infinity scroll

--- systems/frontend/src/ApolloClient.provider.tsx
+++ systems/frontend/src/ApolloClient.provider.tsx
@@ -2,40 +2,7 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { createUploadLink } from 'apollo-upload-client';
 import type { PropsWithChildren } from 'react';
 
-const cache = new InMemoryCache({
-  typePolicies: {
-    Query: {
-      fields: {
-        gameList: {
-          keyArgs: false,
-
-          merge(existing = {}, incoming, { args: { offset } }: any) {
-            const existingEdges = existing.edges?.slice(0) ?? [];
-            const { __typename, edges, pageInfo, totalCount } = incoming;
-            edges.forEach((edge: any, index: any) => {
-              existingEdges[offset + index] = edge;
-            });
-            return {
-              __typename,
-              edges: existingEdges,
-              pageInfo,
-              totalCount,
-            };
-          },
-
-          read(existing = {}, { args: { limit, offset } }: any) {
-            return {
-              __typename: existing.__typename,
-              edges: existing.edges?.slice(offset, offset + limit) ?? [],
-              pageInfo: existing.pageInfo,
-              totalCount: existing.totalCount,
-            };
-          },
-        },
-      },
-    },
-  },
-});
+const cache = new InMemoryCache();
 
 export default function ApolloClientProvider({
   children,
