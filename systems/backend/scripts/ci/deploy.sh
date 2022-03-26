d59c38c3b9a297524ea6f4c5822b1d55d89b8fdb
David Ng <david.ng.dev@gmail.com>
2022-03-26 21:21:43 +0000

    ci: deploy when merge to master

--- systems/backend/scripts/ci/deploy.sh
+++ systems/backend/scripts/ci/deploy.sh
@@ -5,7 +5,7 @@ tag=${GITHUB_SHA:-testing}
 ecr_repo=${ECR_REPO}
 ecr_image_name=${ECR_IMAGE_NAME}
 lambda_function_arn=${LAMBDA_FUNCTION_ARN}
-aws_region=${AWS_REGION}
+aws_region=${AWS_DEFAULT_REGION}
 aws ecr get-login-password --region "$aws_region" | docker login --username AWS --password-stdin "$ecr_repo"
 docker build -t backend:latest -t "backend:$tag" -f ./Dockerfile.lambda .
 docker tag  backend:latest "$ecr_repo/$ecr_image_name:latest"
