8a7a088b47d82dbabf42a8d2b96e475480dc1a18
David Ng <david.ng.dev@gmail.com>
2022-03-27 22:49:45 +0100

    feat: enable lambda provisioned concurrency

--- systems/backend/scripts/ci/deploy.sh
+++ systems/backend/scripts/ci/deploy.sh
@@ -5,6 +5,8 @@ tag=${GITHUB_SHA:-testing}
 ecr_repo=${ECR_REPO}
 ecr_image_name=${ECR_IMAGE_NAME}
 lambda_function_arn=${LAMBDA_FUNCTION_ARN}
+lambda_function_latest_version_alias_name=${LAMBDA_FUNCTION_LATEST_VERSION_ALIAS_NAME}
+
 aws_region=${AWS_DEFAULT_REGION}
 aws ecr get-login-password --region "$aws_region" | docker login --username AWS --password-stdin "$ecr_repo"
 docker build -t backend:latest -t "backend:$tag" -f ./Dockerfile.lambda .
@@ -12,7 +14,11 @@ docker tag  backend:latest "$ecr_repo/$ecr_image_name:latest"
 docker tag  "backend:$tag" "$ecr_repo/$ecr_image_name:$tag"
 docker push "$ecr_repo/$ecr_image_name:latest"
 docker push "$ecr_repo/$ecr_image_name:$tag"
-aws lambda update-function-code \
+latest_version=$(aws lambda update-function-code \
     --function-name "$lambda_function_arn" \
     --image-uri "$ecr_repo/$ecr_image_name:$tag" \
-    --publish
+    --publish | jq -r '.Version')
+aws lambda update-alias \
+    --function-name "$lambda_function_arn" \
+    --function-version "$latest_version" \
+    --name "$lambda_function_latest_version_alias_name"
