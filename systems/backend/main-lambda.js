ad283c3a6aaf38a81f267f53167af4e37b28d05d
David Ng <david.ng.dev@gmail.com>
2022-04-02 23:20:59 +0100

    feat: use webpack on lambda function

--- systems/backend/main-lambda.js
+++ systems/backend/main-lambda.js
@@ -1,18 +1,5 @@
-const { bootstrap } = require('./dist/bootstrap');
-const serverlessExpress = require('@vendia/serverless-express');
-
-let server;
-
-async function createServerlessExpress() {
-  const app = await bootstrap();
-  await app.init();
-  const expressApp = app.getHttpAdapter().getInstance();
-  return serverlessExpress({ app: expressApp });
-}
+const { handler } = require('./dist/main.lambda');
 
 module.exports = {
-  async handler(event, context, callback) {
-    server = server ?? (await createServerlessExpress());
-    return server(event, context, callback);
-  },
+  handler,
 };
